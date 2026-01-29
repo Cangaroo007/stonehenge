51a3e4727c17f8a7db7d14c68c29ecabf1a42b0c
Sean Stone <seanstone@seans-air-2.lan>
2026-01-29 10:24:33 -0800

    feat: Add 40mm lamination strip support

--- prisma/migrations/20260129100252_add_lamination_summary/migration.sql
+++
@@ -1,4 +0,0 @@
--- Add lamination summary field to SlabOptimization table
--- This stores the summary of lamination strips for 40mm+ pieces
-
-ALTER TABLE "SlabOptimization" ADD COLUMN "laminationSummary" JSONB;
